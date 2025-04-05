5c3647aed85658adfd071970cc6319f7a2fe4734
samerabdallah <smsssamer@live.com>
2025-04-05 03:27:17 +0300

    Update AssemblyInformationalVersion and AssemblyInfoInputs.cache for versioning consistency

--- Xprema.Framework/obj/Debug/net9.0/Xprema.Framework.AssemblyInfo.cs
+++ Xprema.Framework/obj/Debug/net9.0/Xprema.Framework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xprema.Framework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4858b2617f5170fd6011b61a6cd7dbdf0c0d2512")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25876d7eb0c3c5074cfe1be8b3d82ea18b4e43d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xprema.Framework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xprema.Framework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
